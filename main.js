5133755935463c41971910ec529186b8c1649239
Lucas Cabral <cabralz18@outlook.com>
2023-09-18 22:16:50 -0300

    aula 5

--- main.js
+++ main.js
@@ -5,6 +5,20 @@ const form = document.getElementById("form-itens")
 const itensInput = document.getElementById("receber-item")
 const ulItens = document.getElementById("lista-de-itens")
 const ulItensComprados = document.getElementById("itens-comprados")
+const listaRecuperada = localStorage.getItem('listaDeItens')
+
+function atualizaLocalStorage() {
+    localStorage.setItem('listaDeItens', JSON.stringify(listaDeItens))
+}
+
+
+
+if(listaRecuperada) {
+    listaDeItens = JSON.parse(listaRecuperada)
+    mostrarItem()
+} else {
+    listaDeItens = []
+}
 
 form.addEventListener("submit", function (evento) {
     evento.preventDefault()
@@ -91,8 +105,10 @@ function mostrarItem(){
             mostrarItem()
         })
     })  
+    atualizaLocalStorage()
 
 }
+
 
 function salvarEdicao() {
     const itemEditado = document.querySelector(`[data-value="${itemAEditar}"] input[type="text"]`)
